4826401c4b23e5183aabae8a507d406a95d52d5d
Bakó Gábor <bako.gabor@gmail.com>
2018-07-02 08:14:27 +0200

    warmup1

---
+++ warmup1/front_back.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+"""
+Given a string, return a new string where the first and last chars have been exchanged.
+
+front_back('code') → 'eodc'
+front_back('a') → 'a'
+front_back('ab') → 'ba'
+"""
+
+def front_back(str):
+  if len(str)<=1:
+    return str
+  else:
+    first=str[0]
+    last=str[-1]
+    middle=str[1:len(str)-1]
+    return last+middle+first
+
+
+def front_back_original(str):
+    if len(str) <= 1:
+        return str
+
+    mid = str[1:len(str) - 1]  # can be written as str[1:-1]
+
+    # last + mid + first
+    return str[len(str) - 1] + mid + str[0]
