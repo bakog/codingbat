481a3d5e9d8c0d77d5df0ad3443550bc5f092db2
bakog <bako.gabor@gmail.com>
2018-07-03 09:05:11 +0200

    logic-2

---
+++ logic-2/lone_sum.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+"""
+Given 3 int values, a b c, return their sum. However,
+ if one of the values is the same as another of the values, it does not count towards the sum.
+
+lone_sum(1, 2, 3) → 6
+lone_sum(3, 2, 3) → 2
+lone_sum(3, 3, 3) → 0
+"""
+def lone_sum(a, b, c):
+  if a!=b and b!=c and a!=c:
+    return a+b+c
+  elif a==b and a!=c:
+    return c
+  elif a==c and a!=b:
+    return b
+  elif c==b and c!=a:
+    return a
+  else:
+    return 0
